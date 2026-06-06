cbafe295d5ceab48da807b640a08e2e6adef73c0
GITLAB <waleedeid916@gmail.com>
2026-06-06 22:40:00 +0300

    Creates a new NodeJS app please

---
+++ src/app.js
@@ -0,0 +1,13 @@
+function greet(name) {
+
+  return `Hello, ${name}!`; 
+  }
+
+  module.exports = greet; // Ensure function is exportable 
+  // if run directory, print output (useful for debugging) 
+
+  if (require.main === module) {
+
+    console.log(greet("World"));
+  }
+
